45d69b5fbb11ddfa6926f1297826d4e64e1047c7
dlgytjdsla <dlgytjdsla@naver.com>
2026-03-04 15:24:56 +0900

    feat(2-08): 루트 url 만들기

--- sbb/src/main/java/com/mysite/sbb/MainController.java
+++ sbb/src/main/java/com/mysite/sbb/MainController.java
@@ -13,4 +13,10 @@ public class MainController {
         System.out.println("index");
         return "안녕하세요. sbb에 오신 것을 환영합니다.";
     }
+
+    @GetMapping("/")
+    public String root() {
+        // 홈으로 올 경우, 질문 리스트로 리다이렉트 시킴
+        return "redirect:/question/list";
+    }
 }
