f3b486c414fedb365cff96b9fe56cf5c5c0440f7
dlgytjdsla <dlgytjdsla@naver.com>
2026-03-05 17:44:14 +0900

    feat(2-16):질문 등록 기능 추가하기

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,11 +1,12 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.answer.AnswerForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +17,21 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "question_form";
+        }
+
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
+    }
+
     @GetMapping("/list")
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
@@ -24,7 +40,7 @@ public class QuestionController {
     }
 
     @GetMapping(value ="/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute(question);
         return "/question_detail";
